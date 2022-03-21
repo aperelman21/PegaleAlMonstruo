a7a2e2c80f71f10c5e9034599bf381932e9ec0ce
ElChepeCodes <josepe430@gmail.com>
2022-03-21 17:14:58 -0600

    Game file

--- src/Server/Game.java
+++ src/Server/Game.java
@@ -2,7 +2,10 @@ package Server;
 
 import SerializableObjects.Player;
 
+import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectOutput;
+import java.io.ObjectOutputStream;
 import java.net.*;
 import java.util.ArrayList;
 
@@ -11,9 +14,47 @@ public class Game {
     private InetAddress group;
     private MulticastSocket socket;
     private ArrayList<Player> players;
+    private MulticastSocket msocket = null;
 
+    public int getMaxScore() {
+        return maxScore;
+    }
 
+    public void setMaxScore(int maxScore) {
+        this.maxScore = maxScore;
+    }
 
+    public InetAddress getGroup() {
+        return group;
+    }
+
+    public void setGroup(InetAddress group) {
+        this.group = group;
+    }
+
+    public MulticastSocket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(MulticastSocket socket) {
+        this.socket = socket;
+    }
+
+    public ArrayList<Player> getPlayers() {
+        return players;
+    }
+
+    public void setPlayers(ArrayList<Player> players) {
+        this.players = players;
+    }
+
+    public MulticastSocket getMsocket() {
+        return msocket;
+    }
+
+    public void setMsocket(MulticastSocket msocket) {
+        this.msocket = msocket;
+    }
 
     public void initialize(){
         try {
@@ -29,7 +70,33 @@ public class Game {
         }
     }
 
+    private Player getPlayerMaxScore(){
+        Player maxPlayer = null, currentPlayer = null;
+        int maxScore = 0, currentScore;
+        for (int i = 0; i < players.size(); i++){
+            currentPlayer = players.get(i);
+            currentScore = currentPlayer.getPlayerScore();
+            if (currentScore > maxScore){
+                maxScore = currentScore;
+                maxPlayer = currentPlayer;
+            }
+        }
+        return maxPlayer;
+    }
 
+    public void sendWinner() throws IOException{
+        Player winner = getPlayerMaxScore();
+        String victoryMsg = "Gana " + winner.getPlayerId();
+        ByteArrayOutputStream bStream = new ByteArrayOutputStream();
+        ObjectOutput oo = new ObjectOutputStream(bStream);
+        oo.writeObject(victoryMsg);
+        oo.close();
+
+        byte[] m = bStream.toByteArray(); //enviamos la jugada a todos los jugadores activos
+        DatagramPacket messageOut = new DatagramPacket(m, m.length, this.getGroup(), 6789);
+        this.msocket.send(messageOut);
+
+    }
 
 
 
