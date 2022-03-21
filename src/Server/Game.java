c09ce763a8d4022cb98bef1de2acd5e8861d996e
Alexander Perelman <aperelman21@gmail.com>
2022-03-21 16:56:02 -0600

    Changed game.java

--- src/Server/Game.java
+++ src/Server/Game.java
@@ -1,8 +1,36 @@
 package Server;
 
+import SerializableObjects.Player;
+
+import java.io.IOException;
+import java.net.*;
+import java.util.ArrayList;
+
 public class Game {
-    private int gameId;
     private int maxScore;
+    private InetAddress group;
+    private MulticastSocket socket;
+    private ArrayList<Player> players;
+
+
+
+
+    public void initialize(){
+        try {
+            this.group = InetAddress.getByName("228.5.6.7"); //destination multicast group
+            this.socket = new MulticastSocket(49155);
+            this.socket.joinGroup(group);
+
+            this.maxScore = 5;
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+
 
 
 
